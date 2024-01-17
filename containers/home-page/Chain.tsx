ae89ad6ef8842b64af453fd499dcf98049bf3803
SmileS-777 <p.smiles777@gmail.com>
2024-01-17 21:38:54 +0900

    add modal of connect wallet.

--- containers/home-page/Chain.tsx
+++ containers/home-page/Chain.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
 
 type connectProps = {
   chainIcon: string;
@@ -19,8 +21,16 @@ const Chain = ({
   actionText,
   TokenText,
   isBallance = false,
-  isConnected = false,
 }: connectProps) => {
+  const { openConnectModal } = useConnectModal();
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
+
+  const handleMax = () => {
+    alert("handle max");
+  };
   return (
     <div className="lg:flex md:flex sm:flex w-full gap-1">
       <div className="py-2 bg-gray-800 rounded-xl py-5 px-5 mt-2">
@@ -33,13 +43,30 @@ const Chain = ({
         />
         <div className="text-center pt-4">
           <p className="">{chainName}</p>
-          <p className="">{buttonText}</p>
+          {!isConnected ? (
+            <span
+              className="underline hover:cursor-pointer"
+              onClick={openConnectModal}
+            >
+              Connect Wallet
+            </span>
+          ) : (
+            <p className="">{buttonText}</p>
+          )}
         </div>
       </div>
       <div className="grow bg-gray-800 rounded-xl py-5 px-5 mt-2">
         <p className="text-blue-200 text-sm">{actionText}:</p>
         <div className="flex justify-between mt-3">
-          <div className="text-2xl">0.0</div>
+          <div className="text-2xl">
+            {isBallance && (
+              <input
+                type="text"
+                className="bg-transparent w-20 outline-none"
+                autoFocus
+              ></input>
+            )}
+          </div>
           <div className="flex justify-between ">
             <Image src={tokenIcon} alt="lyx" />
             <div className="ml-2 mt-1">{TokenText}</div>
@@ -49,7 +76,15 @@ const Chain = ({
           <div className="flex text-blue-200 text-sm">
             <div className="mr-auto">$0.0</div>
             {isBallance ? (
-              <div className="ml-auto">Balance: 46.332 Max</div>
+              <div className="ml-auto">
+                Balance: 46.332
+                <span
+                  className="underline ml-2 hover:cursor-pointer"
+                  onClick={handleMax}
+                >
+                  Max
+                </span>
+              </div>
             ) : (
               ""
             )}
---
+++ containers/home-page/ConnectButton.tsx
@@ -0,0 +1,38 @@
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
+
+const ConnectButton = () => {
+  const { openConnectModal } = useConnectModal();
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
+
+  const swap = () => {
+    // swap logic
+  };
+
+  return (
+    <div className="mt-4">
+      {!isConnected ? (
+        <button
+          onClick={openConnectModal}
+          type="button"
+          className="bg-indigo-700  text-white px-5 py-3 me-2 mb-2 text-sm rounded-lg hover:bg-blue-900 w-full"
+        >
+          Connect Wallet
+        </button>
+      ) : (
+        <button
+          onClick={swap}
+          type="button"
+          className="bg-indigo-700  text-white px-5 py-3 me-2 mb-2 text-sm rounded-lg hover:bg-blue-900 w-full"
+        >
+          swap
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ConnectButton;
